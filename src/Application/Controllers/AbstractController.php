33c3133a1a6f589d682f944a6e0c97ca92569f6a
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2023-10-18 17:42:43 +0700

    Add twig template engine

--- src/Application/Controllers/AbstractController.php
+++ src/Application/Controllers/AbstractController.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace App\Application\Controllers;
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 abstract class AbstractController
 {
     public function __invoke()
@@ -12,4 +15,16 @@ abstract class AbstractController
     }
 
     abstract protected function view(): void;
+
+    protected function render(string $template, array $ctx = []): void
+    {
+        echo self::twigEnv()->render($template, $ctx);
+    }
+
+    private static function twigEnv()
+    {
+        $loader = new FilesystemLoader(__DIR__ . '/../../../templates');
+        $twig = new Environment($loader, []);
+        return $twig;
+    }
 }
