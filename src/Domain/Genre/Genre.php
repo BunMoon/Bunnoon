735ed3403e09ed083311521aed7f695188792796
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-04 16:02:11 +0700

    Assign methods into genre domain

--- src/Domain/Genre/Genre.php
+++ src/Domain/Genre/Genre.php
@@ -6,7 +6,27 @@ namespace App\Domain\Genre;
 
 class Genre
 {
-    public function __construct()
+    public function __construct(private array $record)
     {
+    }
+
+    public function id(): int
+    {
+        return $this->record['id'];
+    }
+
+    public function enName(): string
+    {
+        return $this->record['enName'];
+    }
+
+    public function thName(): string
+    {
+        return $this->record['thName'];
+    }
+
+    public function title(): string
+    {
+        return $this->record['title'];
     }
 }
