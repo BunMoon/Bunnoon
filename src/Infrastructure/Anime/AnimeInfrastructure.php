15ad93b4cab9e253019241c7d5c627a81c624daa
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-04 23:44:43 +0700

    Create anime infrastructure

---
+++ src/Infrastructure/Anime/AnimeInfrastructure.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Anime;
+
+use App\Domain\Anime\AnimeRepository;
+use App\Infrastructure\Infrastructure;
+
+class AnimeInfrastructure extends Infrastructure implements AnimeRepository
+{
+}
