2fd67912821befb6ad151c970356910146b54114
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-04 17:48:50 +0700

    Update list genre controller and template
    Add genre repository and forloop genre link element

--- src/Application/Controllers/Genre/ListGenreController.php
+++ src/Application/Controllers/Genre/ListGenreController.php
@@ -5,14 +5,26 @@ declare(strict_types=1);
 namespace App\Application\Controllers\Genre;
 
 use App\Application\Controllers\AbstractController;
+use App\Domain\Genre\GenreRepository;
+use App\Infrastructure\Genre\GenreInfrastructure;
 
 class ListGenreController extends AbstractController
 {
+    private GenreRepository $genreRepository;
+
+    public function __construct()
+    {
+        $this->genreRepository = new GenreInfrastructure();
+    }
+
     /**
      * {@inheritDoc}
      */
     protected function view(): void
     {
-        $this->render('ListGenre.html');
+        $genres = $this->genreRepository->find();
+        $this->render('ListGenre.html', [
+            'genres' => $genres
+        ]);
     }
 }
