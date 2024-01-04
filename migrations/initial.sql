17ae1c4f9d513866600574e369dadfd77a2dfd76
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-04 15:51:56 +0700

    Update initial.sql at migrations
    Create and Initial genre table

--- migrations/initial.sql
+++ migrations/initial.sql
@@ -1,2 +1,22 @@
 -- create database
 CREATE DATABASE bunnoon CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+-- create genres table
+CREATE TABLE `bunnoon`.`genres` (`id` INT NOT NULL AUTO_INCREMENT , `enName` VARCHAR(50) NOT NULL , `thName` VARCHAR(50) NOT NULL , `title` VARCHAR(50) NOT NULL , PRIMARY KEY (`id`), UNIQUE (`title`)) ENGINE = InnoDB;
+
+-- initial genre records
+INSERT INTO `bunnoon`.`genres` (enName, thName, title)
+VALUES
+    ('Action', 'ต่อสู้', 'action'),
+    ('Adventure', 'ผจญภัย', 'adventure'),
+    ('Comedy', 'บันเทิง', 'comedy'),
+    ('Drama', 'ดราม่า', 'drama'),
+    ('Slice of Life', 'ชีวิตประจำวัน', 'slice+of+life'),
+    ('Fantasy', 'แฟนตาซี', 'fantasy'),
+    ('Magic', 'เวทมนต์', 'magic'),
+    ('Supernatural', 'เหนือธรรมชาติ', 'supernatural'),
+    ('Horror', 'สยองขวัญ', 'horror'),
+    ('Mystery', 'ลี้ลับ', 'mystery'),
+    ('Psychological', 'จิตวิทยา', 'psychological'),
+    ('Romance', 'ความรัก', 'romance'),
+    ('Sci-Fi', 'ก้าวข้ามวิทยาศาต์', 'sci-fi');
