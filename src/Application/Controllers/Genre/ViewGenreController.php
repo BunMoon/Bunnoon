673fd6b9b342dae4eae850ce16acab17b87fbb79
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-05 00:01:48 +0700

    Update view genre controller
    Add findByGenreId method by anime repository

--- src/Application/Controllers/Genre/ViewGenreController.php
+++ src/Application/Controllers/Genre/ViewGenreController.php
@@ -5,17 +5,21 @@ declare(strict_types=1);
 namespace App\Application\Controllers\Genre;
 
 use App\Application\Controllers\AbstractController;
+use App\Domain\Anime\AnimeRepository;
 use App\Domain\Genre\GenreNotFoundException;
 use App\Domain\Genre\GenreRepository;
+use App\Infrastructure\Anime\AnimeInfrastructure;
 use App\Infrastructure\Genre\GenreInfrastructure;
 
 class ViewGenreController extends AbstractController
 {
     private GenreRepository $genreRepository;
+    private AnimeRepository $animeRepository;
 
     public function __construct()
     {
         $this->genreRepository = new GenreInfrastructure();
+        $this->animeRepository = new AnimeInfrastructure();
     }
 
     /**
@@ -25,8 +29,10 @@ class ViewGenreController extends AbstractController
     {
         try {
             $genre = $this->genreRepository->findOneByTitle($_GET['id']);
+            $animes = $this->animeRepository->findByGenreId($genre->id());
             $this->render('ViewGenre.html', [
                 'genre' => $genre,
+                'animes' => $animes,
             ]);
         } catch (GenreNotFoundException) {
             echo 'Genre does not exist.';
