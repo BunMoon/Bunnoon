6d08c4fb0f160f2166fdc0e2917aef62d4ce98dd
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-04 15:30:12 +0700

    Create genre infrastructure

---
+++ src/Infrastructure/Genre/GenreInfrastructure.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Genre;
+
+use App\Domain\Genre\GenreRepository;
+use App\Infrastructure\Infrastructure;
+
+class GenreInfrastructure extends Infrastructure implements GenreRepository
+{
+}
