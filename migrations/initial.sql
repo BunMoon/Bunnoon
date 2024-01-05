bfccc8eb528cc8c6995f51ea6b1c08113c28d188
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-05 14:02:59 +0700

    Update initial.sql into migrations
    Create anime_episodes table and AnimeEpisodesToAnime relation

--- migrations/initial.sql
+++ migrations/initial.sql
@@ -29,6 +29,24 @@ FOREIGN KEY ( `genreId` ) REFERENCES `genres` ( `id` )
 ON DELETE SET NULL
 ON UPDATE NO ACTION;
 
+-- create anime_episodes table
+CREATE TABLE `bunnoon`.`anime_episodes` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `episode` INT NOT NULL,
+    `sound` ENUM ( 'JP', 'TH', 'ALL' ) NOT NULL,
+    `subtitle` ENUM ( 'EN', 'TH', 'ALL' ),
+    `source` VARCHAR( 36 ) NOT NULL,
+    `animeId` INT NOT NULL,
+    PRIMARY KEY ( `id` ),
+    UNIQUE ( `source` )
+) ENGINE = InnoDB;
+
+-- create foreign key - episodes to anime (ManyToOne)
+ALTER TABLE `bunnoon`.`anime_episodes` ADD CONSTRAINT `AnimeEpisodesToAnime`
+FOREIGN KEY ( `animeId` ) REFERENCES `bunnoon`.`animes` ( `id` )
+ON DELETE CASCADE
+ON UPDATE NO ACTION;
+
 -- initial genre records
 INSERT INTO `bunnoon`.`genres` (enName, thName, title)
 VALUES
