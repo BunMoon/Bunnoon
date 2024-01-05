23300a145ce99f4ccffee67c0bb24590e03481e0
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-05 14:19:18 +0700

    Add findByAnimeId method into episode repository

--- src/Domain/Episode/EpisodeRepository.php
+++ src/Domain/Episode/EpisodeRepository.php
@@ -6,4 +6,10 @@ namespace App\Domain\Episode;
 
 interface EpisodeRepository
 {
+    /**
+     * Find many episode records by anime primary key.
+     * 
+     * @return Episode[]
+     */
+    public function findByAnimeId(int $animeId): array;
 }
--- src/Infrastructure/Anime/AnimeEpisodeInfrastructure.php
+++ src/Infrastructure/Anime/AnimeEpisodeInfrastructure.php
@@ -4,9 +4,25 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Anime;
 
+use App\Domain\Episode\Episode;
 use App\Domain\Episode\EpisodeRepository;
 use App\Infrastructure\Infrastructure;
 
 class AnimeEpisodeInfrastructure extends Infrastructure implements EpisodeRepository
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function findByAnimeId(int $animeId): array
+    {
+        $episodes = $this->db->query(
+            "SELECT * FROM anime_episodes WHERE animeId=?",
+            [$animeId]
+        );
+        $result = [];
+        foreach ($episodes as $episode) {
+            array_push($result, new Episode($episode));
+        }
+        return array_values($result);
+    }
 }
