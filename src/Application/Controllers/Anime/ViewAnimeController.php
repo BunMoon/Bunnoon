925165b61c6692c655a78ff03a5a26f839b2d933
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-05 14:31:33 +0700

    Update view anime controller and template
    Add findByAnimeId by episode repository and forloop link episode element

--- src/Application/Controllers/Anime/ViewAnimeController.php
+++ src/Application/Controllers/Anime/ViewAnimeController.php
@@ -7,15 +7,19 @@ namespace App\Application\Controllers\Anime;
 use App\Application\Controllers\AbstractController;
 use App\Domain\Anime\AnimeNotFoundException;
 use App\Domain\Anime\AnimeRepository;
+use App\Domain\Episode\EpisodeRepository;
+use App\Infrastructure\Anime\AnimeEpisodeInfrastructure;
 use App\Infrastructure\Anime\AnimeInfrastructure;
 
 class ViewAnimeController extends AbstractController
 {
     private AnimeRepository $animeRepository;
+    private EpisodeRepository $episodeRepository;
 
     public function __construct()
     {
         $this->animeRepository = new AnimeInfrastructure();
+        $this->episodeRepository = new AnimeEpisodeInfrastructure();
     }
 
     /**
@@ -25,8 +29,10 @@ class ViewAnimeController extends AbstractController
     {
         try {
             $anime = $this->animeRepository->findOneByTitle($_GET['id']);
+            $episodes = $this->episodeRepository->findByAnimeId($anime->id());
             $this->render('ViewAnime.html', [
                 'anime' => $anime,
+                'episodes' => $episodes,
             ]);
         } catch (AnimeNotFoundException) {
             echo 'Anime does not exist.';
