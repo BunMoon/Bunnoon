9b3a517c400d4345f15d4afbbff494d73b024ed0
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-05 14:14:49 +0700

    Create episode infrastructure

---
+++ src/Infrastructure/Anime/AnimeEpisodeInfrastructure.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Infrastructure\Anime;
+
+use App\Domain\Episode\EpisodeRepository;
+use App\Infrastructure\Infrastructure;
+
+class AnimeEpisodeInfrastructure extends Infrastructure implements EpisodeRepository
+{
+}
