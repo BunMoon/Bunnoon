4af283397abc31a0613fa9a98d8ea29fcb3b0cb7
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-06 15:54:55 +0700

    Update signup controller and template
    Add save method by user repository
    Create signup form with post method

--- src/Application/Controllers/Auth/SignupController.php
+++ src/Application/Controllers/Auth/SignupController.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 namespace App\Application\Controllers\Auth;
 
 use App\Application\Controllers\AbstractController;
+use App\Domain\User\UserRepository;
+use App\Infrastructure\User\UserInfrastructure;
 
 class SignupController extends AbstractController
 {
+    private UserRepository $userRepository;
+
     public function __construct()
     {
+        $this->userRepository = new UserInfrastructure();
     }
 
     /**
@@ -17,6 +22,17 @@ class SignupController extends AbstractController
      */
     protected function view(): void
     {
+        if (isset($_POST['request'])) {
+            $this->userRepository->save($_POST['email'], $_POST['username'], $_POST['password']);
+            $this->redirect();
+            return;
+        }
         $this->render('Signup.html');
+    }
+
+    private function redirect(): void
+    {
+        $url = $_GET['continue'] ?? '/';
+        header("Location: $url");
     }
 }
