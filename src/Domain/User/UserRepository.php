56233b285c9f3fef44a5165dec39e241c20d8a88
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-06 15:53:42 +0700

    Add save method into user repository

--- src/Domain/User/UserRepository.php
+++ src/Domain/User/UserRepository.php
@@ -6,4 +6,8 @@ namespace App\Domain\User;
 
 interface UserRepository
 {
+    /**
+     * Create user record.
+     */
+    public function save(string $email, string $username, string $password): void;
 }
--- src/Infrastructure/User/UserInfrastructure.php
+++ src/Infrastructure/User/UserInfrastructure.php
@@ -9,4 +9,17 @@ use App\Infrastructure\Infrastructure;
 
 class UserInfrastructure extends Infrastructure implements UserRepository
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function save(string $email, string $username, string $password): void
+    {
+        $stamt = $this->db->pdo->prepare("INSERT INTO users (email, username, password, name) VALUES (?, ?, ?, ?)");
+        $stamt->execute([
+            $email,
+            strtolower($username),
+            password_hash($password, PASSWORD_ARGON2ID),
+            $username,
+        ]);
+    }
 }
