c34db3829940cad1c0f6a2d9c13cf4d37caae861
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-08 15:49:22 +0700

    Create uniqid method into random utilities

---
+++ utils/Random.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Utilities;
+
+use Exception;
+
+class Random
+{
+    /**
+     * Generate a unique ID.
+     * 
+     * @return string
+     */
+    public static function uniqid(int $length): string
+    {
+        if (function_exists('random_bytes')) {
+            $bytes = random_bytes(ceil($length / 2));
+        } elseif (function_exists('openssl_random_pseudo_bytes')) {
+            $bytes = openssl_random_pseudo_bytes(ceil($length / 2));
+        } else {
+            throw new Exception('no cryptographically secure random function available');
+        }
+        return substr(bin2hex($bytes), 0, $length);
+    }
+}
