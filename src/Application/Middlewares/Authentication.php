62a68b03d9c75c0460dd8fcdada4c77c4bee1010
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-08 19:24:57 +0700

    Update authentication middleware
    Update login method and is authenticated check

--- src/Application/Middlewares/Authentication.php
+++ src/Application/Middlewares/Authentication.php
@@ -4,15 +4,24 @@ declare(strict_types=1);
 
 namespace App\Application\Middlewares;
 
+use App\Domain\Session\SessionRepository;
 use App\Domain\User\User;
+use App\Infrastructure\Session\SessionInfrastructure;
 use Library\Strategy;
+use Utilities\Random;
 
 class Authentication extends Strategy
 {
     public bool $isAuthenticated = false;
 
+    private SessionRepository $sessionRepository;
+
     public function __construct()
     {
+        $this->sessionRepository = new SessionInfrastructure();
+        if (isset($_COOKIE['LOGIN_INFO'])) {
+            $this->isAuthenticated = true;
+        }
     }
 
     public function authenticate(array $value, int $strategy = 0): bool
@@ -27,6 +36,16 @@ class Authentication extends Strategy
 
     public function login(User $user): void
     {
+        $token = Random::uniqid();
+        $this->sessionRepository->save($token, $user->id());
+        setcookie('LOGIN_INFO', $token, [
+            'expires' => time() + 31536000,
+            'path' => '/',
+            'domain' => '',
+            'secure' => true,
+            'httponly' => true,
+            'samesite' => 'Strict',
+        ]);
         echo 'Login successfully!';
     }
 }
