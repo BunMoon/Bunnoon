14d53048e426af49c3cdc174c244ba3dbee9d156
Phuchon Chueapong <145848811+BunMoon@users.noreply.github.com>
2024-01-08 19:48:38 +0700

    Update authorization middleware
    Add searchUserSession method

--- src/Application/Middlewares/Authorization.php
+++ src/Application/Middlewares/Authorization.php
@@ -4,14 +4,37 @@ declare(strict_types=1);
 
 namespace App\Application\Middlewares;
 
+use App\Domain\Session\SessionNotFoundException;
+use App\Domain\Session\SessionRepository;
 use App\Domain\User\User;
+use App\Domain\User\UserNotFoundException;
+use App\Domain\User\UserRepository;
+use App\Infrastructure\Session\SessionInfrastructure;
+use App\Infrastructure\User\UserInfrastructure;
 
 class Authorization
 {
-    public User $user;
+    public User|null $user = null;
+
+    private SessionRepository $sessionRepository;
+    private UserRepository $userRepository;
 
     public function __construct()
     {
-        $this->user = new User([]);
+        $this->sessionRepository = new SessionInfrastructure();
+        $this->userRepository = new UserInfrastructure();
+    }
+
+    public function searchUserSession()
+    {
+        try {
+            $session = $this->sessionRepository->findOneByToken($_COOKIE['LOGIN_INFO']);
+            $user = $this->userRepository->findOneById($session->userId());
+            $this->user = $user;
+        } catch (SessionNotFoundException) {
+            echo 'Session does not exist.';
+        } catch (UserNotFoundException) {
+            echo 'User does not exist.';
+        }
     }
 }
